4d91d2d5473006ea148169f56964f23339330c7b
Yeasir7 <ibnayeasir@gmail.com>
2025-05-13 21:17:45 +0600

    card design done

--- src/components/job/Job.jsx
+++ src/components/job/Job.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { CiLocationOn } from "react-icons/ci";
+import { CiDollar } from "react-icons/ci";
 
 const Job = ({job}) => {
     const {logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
         <div>
-<div className="card card-compact bg-base-100 shadow-xl">
+<div className="card card-compact bg-base-100 shadow-xl p-8">
   <div>
       <img
       src={logo}
@@ -16,6 +18,16 @@ const Job = ({job}) => {
     <div className='flex gap-3 my-6'>
       <button className='px-5 py-2 border-2 rounded-lg'>{remote_or_onsite}</button>
       <button className='px-5 py-2 border-2 rounded-lg'>{job_type}</button>
+    </div>
+    <div className='flex gap-4 mb-7' >
+      <h2 className='flex gap-2 items-center justify-center'>
+        <CiLocationOn />
+        {location}
+      </h2>
+      <h2 className='flex gap-2 items-center justify-center'>
+        <CiDollar />
+        {salary}
+      </h2>
     </div>
     <div className="card-actions">
       <button className="btn btn-primary">View Details</button>
