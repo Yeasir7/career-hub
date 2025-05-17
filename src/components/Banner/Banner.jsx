1b3cc79b7d908204538fed6fba486238ca944434
Yeasir7 <ibnayeasir@gmail.com>
2025-05-17 09:56:55 +0600

    added hero

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import user from "../../assets/images/user.png"
 
 const Banner = () => {
     return (
-        <div>
-            <h3 className='text-5xl'>One Step Closer To Your Dream Job</h3>
+        <div className='my-10'>
+<div className="hero bg-base-200 rounded-2xl">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img
+      src={user}
+      className="max-w-sm rounded-lg shadow-2xl"
+    />
+    <div className='mt-11 mb-32 mx-5'>
+      <h1 className="text-5xl font-bold">One Step Closer To Your Dream Job</h1>
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
